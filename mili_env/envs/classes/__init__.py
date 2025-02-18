df2f74d4f4394890c8a046223b10c5f0adca99fc
Paul Zeinaty <zeinatypol@gmail.com>
2025-02-18 14:19:34 +0100

    Refactor AgentConfig class

--- mili_env/envs/classes/__init__.py
+++ mili_env/envs/classes/__init__.py
@@ -3,7 +3,7 @@
 This module contains the classes for the mili_env package.
 """
 
-from mili_env.envs.classes.model import QTrainer  # noqa: F401
-from mili_env.envs.classes.qlearning_agent import AgentConfig, QLearningAgent  # noqa: F401
+from mili_env.envs.classes.agent_config import AgentConfig  # noqa: F401
+from mili_env.envs.classes.qlearning_agent import QLearningAgent  # noqa: F401
 from mili_env.envs.classes.robot_base import RobotBase  # noqa: F401
 from mili_env.envs.classes.terrain import Terrain  # noqa: F401
